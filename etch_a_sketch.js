9c2711ef8388654932c5604a401cc65e3748c354
Levell99RC <vincentsong96@gmail.com>
2023-08-01 17:10:31 -0400

    Add color change when hovering over grid

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -16,11 +16,25 @@ for (let a = 1; a <= 16; a++) {
         box.classList.add(`box${((a - 1) * 16) + b}`);
         box.style.width = '20px';
         box.style.height = '20px';
-        box.style.color = 'white';
-        box.style.border = '2px solid black';
+        box.style.backgroundColor = 'white';
+        box.style.border = '1px solid black';
+        box.addEventListener('mouseover', changeColorMouseover);
+        box.addEventListener('mouseout', changeColorMouseout);
         row.appendChild(box);
     }
     grid.appendChild(row);
 }
 
 body.appendChild(grid);
+
+function changeColorMouseover() {
+    this.style.backgroundColor = 'red';
+    this.style.transitionDelay = '0s';
+    this.style.transition = '0s';
+}
+
+function changeColorMouseout() {
+    this.style.backgroundColor = 'white';
+    this.style.transitionDelay = '0.5s';
+    this.style.transition = '1.5s';
+}
