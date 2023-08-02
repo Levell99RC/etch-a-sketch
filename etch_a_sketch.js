a60402c74a5a32a7c141eda7c5a382ea81daeb5c
Levell99RC <vincentsong96@gmail.com>
2023-08-02 11:19:00 -0400

    Encapsulate code to make grid into a function

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,26 +1,33 @@
 const body = document.querySelector('body');
-const grid = document.createElement('div');
-grid.classList.add('grid');
 
-for (let a = 1; a <= 16; a++) {
-    let row = document.createElement('div');
-    row.classList.add(`row${a}`);
-    row.style.display = 'flex';
-    for (let b = 1; b <= 16; b++) {
-        let box = document.createElement('div');
-        box.classList.add(`box${((a - 1) * 16) + b}`);
-        box.style.width = '20px';
-        box.style.height = '20px';
-        box.style.backgroundColor = 'white';
-        box.style.border = '1px solid black';
-        box.addEventListener('mouseover', changeColorMouseover);
-        box.addEventListener('mouseout', changeColorMouseout);
-        row.appendChild(box);
+createGrid();
+
+function createGrid(size = 16) {
+    const grid = document.createElement('div');
+
+    grid.classList.add('grid');
+    grid.style.alignSelf = 'center';
+    
+    for (let a = 1; a <= size; a++) {
+        let row = document.createElement('div');
+        row.classList.add(`row${a}`);
+        row.style.display = 'flex';
+        for (let b = 1; b <= size; b++) {
+            let box = document.createElement('div');
+            box.classList.add(`box${((a - 1) * size) + b}`);
+            box.style.width = '20px';
+            box.style.height = '20px';
+            box.style.backgroundColor = 'white';
+            box.style.border = '1px solid black';
+            box.addEventListener('mouseover', changeColorMouseover);
+            box.addEventListener('mouseout', changeColorMouseout);
+            row.appendChild(box);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
+    
+    body.appendChild(grid);
 }
-
-body.appendChild(grid);
 
 function changeColorMouseover() {
     this.style.backgroundColor = 'red';
